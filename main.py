119b760544976081677ab34998ef4d91ae28c006
ErenKaptan4 <erenkaptanalp@gmail.com>
2025-04-04 15:30:41 +0200

    vercelTest

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from fastapi import FastAPI, File, UploadFile, HTTPException
+from fastapi.params import Depends
 from pydantic import BaseModel
 import motor.motor_asyncio
 app = FastAPI()
 # Connect to Mongo Atlas
-client = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://admin:LZEOpvrC4xaEGg7v@erenkaptan.upt8dcm.mongodb.net/?retryWrites=true&w=majority&appName=ErenKaptan")
-db = client.multimedia_db
+client123 = motor.motor_asyncio.AsyncIOMotorClient("mongodb+srv://admin:LZEOpvrC4xaEGg7v@erenkaptan.upt8dcm.mongodb.net/?retryWrites=true&w=majority&appName=ErenKaptan")
+async def get_database():
+    # Create a new client for each request
+    client = motor.motor_asyncio.AsyncIOMotorClient(
+        client123,
+        maxPoolSize=1,
+        minPoolSize=0,
+        serverSelectionTimeoutMS=5000
+    )
+    try:
+        yield client.multimedia_db
+    finally:
+        client.close()
+
 class PlayerScore(BaseModel):
     player_name: str
     score: int
 @app.post("/upload_sprite")
-async def upload_sprite(file: UploadFile = File(...)):
+async def upload_sprite(file: UploadFile = File(...), db=Depends(get_database)):
 # In a real application, the file should be saved to a storage service
     content =   await file.read()
     sprite_doc = {"filename": file.filename, "content": content}
